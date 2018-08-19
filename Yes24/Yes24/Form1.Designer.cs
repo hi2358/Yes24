f381443cff5a03fc4efd69c134a3f5d44c5d8631
hi2358 <40030463+hi2358@users.noreply.github.com>
2018-08-19 21:52:50 +0900

    Add files via upload

---
+++ Yes24/Yes24/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Yes24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.번호 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.책이름 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(570, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(585, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.번호,
+            this.책이름});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(623, 283);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // 번호
+            // 
+            this.번호.HeaderText = "번호";
+            this.번호.Name = "번호";
+            this.번호.ReadOnly = true;
+            // 
+            // 책이름
+            // 
+            this.책이름.HeaderText = "책이름";
+            this.책이름.Name = "책이름";
+            this.책이름.ReadOnly = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(472, 237);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(159, 81);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Visible = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 323);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 번호;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 책이름;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Yes24/Yes24/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Yes24
+{
+    public partial class Form1 : Form
+    {
+        //yes24Search 전역변수는 button1에서 받아서 webBrowser1_DocumentCompleted에
+        //넘겨줘서 처리하기위해 정의
+        String yes24Search;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String search = textBox1.Text;
+
+            //euc-kr변환
+            String wordsum = null;
+            int euckr = 51949;
+
+            System.Text.Encoding encoding = System.Text.Encoding.GetEncoding(euckr);
+            byte[] euckrbyte = encoding.GetBytes(search);
+            foreach(var b in euckrbyte)
+            {
+                wordsum = wordsum + "%" + Convert.ToString(b, 16);
+            }
+
+            //yes24 검색 파싱 시작
+            yes24Search = "http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&domain=ALL&qdomain=%C0%FC%C3%BC&Wcode=001_005&query=" + wordsum ;
+            webBrowser1.Navigate(yes24Search);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            List<string> resultlist = new List<string>();
+
+            HtmlDocument document = this.webBrowser1.Document;
+                
+            String tableid = "schMid_wrap";
+                
+            HtmlElement cm = document.GetElementById(tableid);
+            HtmlElementCollection trs = cm.GetElementsByTagName("p");
+
+            foreach (HtmlElement el in trs)
+            {
+                if (el.GetAttribute("className") == "goods_name goods_icon")
+                {
+                    el.Focus();
+                    //MessageBox.Show(el.InnerText);
+                    resultlist.Add(el.InnerText);
+                }
+            }
+            int a = 1;
+            //파싱된 데이터 Datagridview1에 삽입
+            foreach (String result in resultlist)
+            {
+                dataGridView1.Rows.Add(a, result);
+                a=a+1;
+            }
+        }
+    }
+}
